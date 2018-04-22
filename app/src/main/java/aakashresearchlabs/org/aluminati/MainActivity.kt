6c38cdfa43fd586e7882752cb82953051cc75b86
Sriram Kailasam <sriramklash@gmail.com>
2018-04-22 10:27:00 +0530

    Setup viewpager and tablayout

--- app/src/main/java/aakashresearchlabs/org/aluminati/MainActivity.kt
+++ app/src/main/java/aakashresearchlabs/org/aluminati/MainActivity.kt
@@ -2,11 +2,44 @@ package aakashresearchlabs.org.aluminati
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.design.widget.TabLayout
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = adapter
+
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+        private val fragmentList: MutableList<Fragment> = ArrayList()
+
+        override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
+            return false
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
+        fun addFragment(fragment: Fragment){
+            fragmentList.add(fragment)
+        }
     }
 }
