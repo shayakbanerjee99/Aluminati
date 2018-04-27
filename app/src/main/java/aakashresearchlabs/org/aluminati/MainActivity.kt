b36c042ce43869c318db1bcdcd5d6929b9e8ae41
Sriram Kailasam <sriramklash@gmail.com>
2018-04-27 19:59:11 +0530

    Move ViewPagerAdapter to separate file

--- app/src/main/java/aakashresearchlabs/org/aluminati/MainActivity.kt
+++ app/src/main/java/aakashresearchlabs/org/aluminati/MainActivity.kt
@@ -2,12 +2,8 @@ package aakashresearchlabs.org.aluminati
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.design.widget.TabLayout
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,30 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.setupViewPager()
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = adapter
 
-        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
         tabLayout.setupWithViewPager(viewPager)
     }
 
-    class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
-        private val fragmentList: MutableList<Fragment> = ArrayList()
-
-        override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
-            return false
-        }
-
-        override fun getCount(): Int {
-            return fragmentList.size
-        }
-
-        override fun getItem(position: Int): Fragment {
-            return fragmentList[position]
-        }
-
-        fun addFragment(fragment: Fragment){
-            fragmentList.add(fragment)
-        }
-    }
 }
