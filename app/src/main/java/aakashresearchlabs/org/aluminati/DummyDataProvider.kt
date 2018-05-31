d351de46a603c6a54f02c621b985d2d3855c7466
Sriram Kailasam <sriramklash@gmail.com>
2018-05-31 22:59:11 +0530

    Renamed a few list items

--- app/src/main/java/aakashresearchlabs/org/aluminati/DummyDataProvider.kt
+++ app/src/main/java/aakashresearchlabs/org/aluminati/DummyDataProvider.kt
@@ -4,12 +4,12 @@ class DummyDataProvider{
     companion object {
         fun dummyAlumList(): ArrayList<Alum> {
             return arrayListOf(
-                    Alum("x y", 18),
+                    Alum("X Y", 18),
                     Alum("John Doe", 38),
                     Alum("Jane Doe", 68),
-                    Alum("Crazy Guy", 28),
-                    Alum("Mad Dog", 14),
-                    Alum("Khooni Panja", 44))
+                    Alum("ABC XYZ", 28),
+                    Alum("LMN DEF", 14),
+                    Alum("PQR RPQ", 44))
         }
     }
 }
