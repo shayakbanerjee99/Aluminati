b95bfe6ded10063922652c06ddf0dbaa23c8f1fe
Sriram Kailasam <sriramklash@gmail.com>
2018-05-31 20:23:44 +0530

    Inflate alumItem as LinearLayout

--- app/src/main/java/aakashresearchlabs/org/aluminati/AlumniAdapter.kt
+++ app/src/main/java/aakashresearchlabs/org/aluminati/AlumniAdapter.kt
@@ -2,16 +2,19 @@
 package aakashresearchlabs.org.aluminati
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
 class AlumniAdapter(private val alumniList: ArrayList<Alum>) : RecyclerView.Adapter<AlumHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlumHolder {
-        val alumItem = LayoutInflater.from(parent.context).inflate(R.layout.alum_item, parent, false)
+        val alumItem = LayoutInflater.from(parent.context).inflate(R.layout.alum_item, parent, false) as LinearLayout
         return AlumHolder(alumItem)
     }
 
     override fun getItemCount(): Int {
+        Log.d("aluminati", "getitemcount called")
         return alumniList.size
     }
 
