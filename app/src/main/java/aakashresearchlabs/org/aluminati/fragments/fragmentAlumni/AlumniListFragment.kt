5947ccacb3ac23b4a303b8258c95cb6f37d24cdf
Sriram Kailasam <sriramklash@gmail.com>
2018-07-21 21:01:09 +0530

    Add AlumHolder and AlumniAdapter

--- app/src/main/java/aakashresearchlabs/org/aluminati/fragments/fragmentAlumni/AlumniListFragment.kt
+++ app/src/main/java/aakashresearchlabs/org/aluminati/fragments/fragmentAlumni/AlumniListFragment.kt
@@ -4,9 +4,13 @@ import aakashresearchlabs.org.aluminati.R
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_alumni_list.*
 
 class AlumniListFragment : Fragment() {
@@ -27,5 +31,30 @@ class AlumniListFragment : Fragment() {
         fun newInstance(): AlumniListFragment {
             return AlumniListFragment()
         }
+    }
+}
+
+private class AlumniAdapter(private val alumniList: ArrayList<Alum>) : RecyclerView.Adapter<AlumHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlumHolder {
+        val alumItem = LayoutInflater.from(parent.context).inflate(R.layout.alum_item, parent, false) as LinearLayout
+        return AlumHolder(alumItem)
+    }
+
+    override fun getItemCount(): Int {
+        Log.d("aluminati", "getitemcount called")
+        return alumniList.size
+    }
+
+    override fun onBindViewHolder(holder: AlumHolder, position: Int) {
+        holder.populateHolder(alumniList[position])
+    }
+}
+
+private class AlumHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    // private val alumImage: ImageView = itemView.findViewById(R.id.alum_image)
+    private val alumName: TextView = itemView.findViewById(R.id.alum_name)
+
+    fun populateHolder(alum: Alum) {
+        alumName.text = alum.name
     }
 }
