75ceecb5daa84defc157f4e3b424cac44bdbbe94
Sriram Kailasam <sriramklash@gmail.com>
2018-07-21 21:03:49 +0530

    Move ViewPagerAdapter class to activity file

--- app/src/main/java/aakashresearchlabs/org/aluminati/activities/MainActivity.kt
+++ app/src/main/java/aakashresearchlabs/org/aluminati/activities/MainActivity.kt
@@ -1,13 +1,19 @@
 package aakashresearchlabs.org.aluminati.activities
 
 import aakashresearchlabs.org.aluminati.R
+import aakashresearchlabs.org.aluminati.fragments.chatfragment.ChatFragment
+import aakashresearchlabs.org.aluminati.fragments.fragmentAlumni.AlumniListFragment
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -46,5 +52,30 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(context, ProfileActivity::class.java)
         startActivity(intent)
         return true
+    }
+}
+
+private class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    companion object {
+        const val PAGE_COUNT = 2
+    }
+
+    override fun getCount(): Int {
+        Log.d("aluminati", "getcount called")
+        return PAGE_COUNT
+    }
+
+    override fun getItem(position: Int): Fragment = when(position) {
+        0 -> AlumniListFragment.newInstance()
+        1 -> ChatFragment.newInstance()
+
+        else -> AlumniListFragment.newInstance()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence = when(position) {
+        0 -> "Alumni"
+        1 -> "Chats"
+
+        else -> "Alumni"
     }
 }
